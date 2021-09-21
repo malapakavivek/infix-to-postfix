dbc102855389b52601c9e0ce8332a66ea273be65
malapakavivek <89408748+malapakavivek@users.noreply.github.com>
2021-09-21 11:13:17 +0530

    Update infix_postfix.py

--- infix_postfix.py
+++ infix_postfix.py
@@ -1,3 +1,21 @@
+from tkinter import *
+
+root=Tk()
+root.title("converter")
+root.geometry("500x100")
+
+#Create labels 
+label=Label(root,text="Enter the Infix Equation:").grid(row=0,column=1)
+label1=Label(root,text="The Postfix Equation is:").grid(row=1,column=1)
+
+#Entry box is created
+e=Entry(root,width=40,borderwidth=4)
+e1=Entry(root,width=40,borderwidth=4)
+
+e.grid(row=0,column=2,padx=10,pady=10)
+e1.grid(row=1,column=2,padx=10,pady=10)
+
+#Infix to Postfix Calculator
 class Calculator:
     def __init__(self):
         self.stack = []
@@ -17,30 +35,48 @@ class Calculator:
         self.stack.append(expression)
 
     def inf_to_pref(self, expression):
-        ans = ""  #Creates an empty string to enter operands to it as it enters
-        prec = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3} #Sets the precedence order of the operators
+        ans = ""  
+        prec = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3} 
         for i in expression: 
-            if i.isalpha() or i.isdigit():  #If the incomming element is an alphabet or a number it enters into the empty string
+            if i.isalpha() or i.isdigit():  
                 ans += i         
             
-            elif i == "(":   #If the incomming element is "(" it pushes it into the stack without checking the preference 
+            elif i == "(":   
                 c.push(i)
             
-            elif i == ')':  #If the incomming element is ")" it pops out all the operators until "(" is found
+            elif i == ')': 
                 while self.stack!= 0 and self.stack[-1] != "(":
                     ans+= c.pop()
                 self.pop()
      
             else: 
-                while self.stack and self.stack[-1]!= '(' and prec[i] <= prec[self.top()]: #Checks the precedence order of the incomming 
-                    ans += self.pop()                                                      # with the top operator in the stack
+                while self.stack and self.stack[-1]!= '(' and prec[i] <= prec[self.top()]: 
+                    ans += self.pop()                                                     
                 self.push(i)                                                         
         
-        while self.stack: #Pops all the operator till the stack is empty
+        while self.stack: 
             ans += str(self.pop())
         return ans
  
 
 c = Calculator()
-a=c.inf_to_pref(input("Enter the Infix Expression: "))
-print("The Postfix Expression is",a)
+
+#Function for what to do after clicking the button
+def click():
+    a=c.inf_to_pref(e.get())
+    ans=e1.delete(0,END)
+    ans=e1.insert(0,a)
+
+def clear():
+    e.delete(0,END)
+    e1.delete(0,END)
+
+#Button is created 
+button=Button(root,text="Enter",command=click)
+button.grid(row="0",column="3",padx=10,pady=10)
+
+b2=Button(root,text="Clear",command=clear)
+b2.grid(row=1,column=3,padx=10,pady=10)
+
+
+root.mainloop()
